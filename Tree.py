c11081ccbe60270b954a571bf8490331894aa6cf
pblml <philipp.t.blueml@gmail.com>
2022-07-06 15:43:54 +0200

    cleaning documenting changing main

---
+++ Tree.py
@@ -0,0 +1,232 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from Simulation import *
+import re
+
+class Node():
+
+    def __init__(self, name, value, t=None, parents=[]):
+        self.name = name
+        self.value = value
+        self.t = self.set_t(t)
+        self.parents = parents
+        #children dict of form {node: probability}
+        self.children = {}
+        self.ev = ""
+
+    def get_option_value(self, strike, discount, opt, type):
+        """
+        Calculate the option value of a node
+
+        self: Node object
+        strike: Strike price of the option
+        discount factor: discount factor
+        opt: "put" or "call"
+        type: "european" or "american"
+        """
+
+        if len(self.children) > 0:
+            s = discount*sum([child.ev*self.children[child] for child in self.children.keys()])
+            exercise_value = (strike-self.value) if type in ["p", "put"] else (self.value-strike)
+            self.ev = max(s, exercise_value) if opt == "american" else s
+        else:
+            if type in ["p", "put"]:
+                self.ev = max(0.0, strike-self.value)
+            elif type in ["c", "call"]:
+                self.ev = max(0.0, self.value-strike)
+        return self
+
+    def add_parent(self, *kwargs):
+        """
+        Method to add parents to a node
+
+        Parameters
+        ----------
+        self:
+            Node object to append to
+        *kwargs:
+            Parent node given as a tuple of (parent_node, probability)
+        """
+
+        for parent, prob in kwargs:
+            self.parents=list(self.parents)
+            self.parents.append(parent)
+            self.parents = list(set(self.parents))
+            parent.children[self] = prob
+
+        return self
+
+    def set_t(self, t):
+        """
+        Set the time period for a node
+
+        Sets the time period when a node gets initialized. If the Nodes `name`-attribute contains a 't[0-9]'-string
+        it is used to set the time
+
+        Parameters
+        ----------
+        self: Node object
+        t: int
+            hard coded t
+        """
+
+        if re.search("t[0-9]+", str(self.name)):
+            # TODO: regex extraction instead of using last character of `self.name`
+            return int(self.name[-1])
+        else:
+           return t
+
+    def __hash__(self):
+        return hash(str(self.name))
+    
+    def __eq__(self, other):
+        return(self.name == other.name and self.value == other.value)
+
+    def __str__(self):
+        return(f"Name: {self.name}\nValue: {self.value}\nParents: {self.parents}\nChildren: {self.children}\nExpected value: {self.ev}")
+
+    def __repr__(self):
+        return(self.name)
+        
+class Tree():
+
+    def __init__(self, nodes=[]):
+        self.nodes = self.append_node()
+
+    def append_node(self, *kwargs):
+        for n in kwargs:
+            globals()[n].tree = self
+            self.nodes.append(n)
+        try:
+            isinstance(self.nodes, list)
+        except Exception as e:
+            print(e)
+            return []
+        return self
+
+    def from_edgelist(self, file):
+        self.nodes = []
+        self.edgelist = pd.read_csv(file)
+        all_nodes = set(pd.concat([self.edgelist["parent"], self.edgelist["child"]], axis=0))
+
+        for idx, row in self.edgelist.iterrows():
+            globals()[row["parent"]] = Node(row["parent"], row["par_value"])
+            globals()[row["child"]] = Node(row["child"], row["ch_value"])
+
+        for idx, row in self.edgelist.iterrows():
+            # TODO: Get globals() out!
+            globals()[row["child"]].add_parent((globals()[row["parent"]], row["prob"]))
+
+        for node in all_nodes:
+            self.append_node(node)
+
+        return self
+
+    def from_S(self, S):
+
+        self.nodes = [] #clear old node list
+        self.edgelist = S.edgelist # get the edgelist attribute of the Simulation object
+
+        # get a list of all nodes in S
+        all_nodes = set(pd.concat([self.edgelist["parent"], self.edgelist["child"]], axis=0))
+
+        # create node for every parent and child in S.edgelist
+        for idx, row in self.edgelist.iterrows():
+            globals()[row["parent"]] = Node(row["parent"], row["par_value"], row["position_p"][0])
+            globals()[row["child"]] = Node(row["child"], row["ch_value"], row["position_c"][0])
+
+        # add the parents to each node
+        for idx, row in self.edgelist.iterrows():
+            # TODO: Get globals() out!
+            globals()[row["child"]].add_parent((globals()[row["parent"]], row["prob"]))
+
+        # append all nodes to the tree
+        for node in all_nodes:
+            self.append_node(node)
+
+        return self
+
+    def get_leafs(self):
+        """
+        Helper method to get the leafs of the Tree.
+        
+        Used to find the starting point of the `calc_options_value`
+        
+        Returns
+        -------
+        list
+            List of leaf nodes
+        """
+
+        leafs = []
+        for node in self.nodes:
+            if len(globals()[node].children) == 0:
+                leafs.append(node)
+        return leafs
+
+    def calc_option_value(self, strike, discount, type="call", opt="european"):
+        """
+        Calculation of option values for all nodes in the tree.
+
+        Parameters
+        ----------
+        self: Tree Object
+        strike: float
+            Strike price of the option
+        discount: float
+            discount factor calculated by `exp(-`
+        type: {"call", "put"}
+        opt: {"european", "american"}
+        """
+        
+        #get the leafs to start calculation
+        parent_list = self.get_leafs()
+
+        # while the parent list is not empty...
+        while len(parent_list) != 0:
+            tmp_parent_list = [] # initialize list for parents of currently calculating nodes
+            for node in parent_list:
+                globals()[node].get_option_value(strike, discount, opt, type)
+                tmp_parent_list.extend([i.name for i in globals()[node].parents])
+            parent_list=list(set(tmp_parent_list)) # set the previous timesteps nodes as the new parent_list
+        return self
+
+    def plot(self):
+        """
+        Generate and show the plot of the Tree() object using its `nodes` attribute.
+
+        If `calc_option_value()` was executed on the tree, it also shows the option value for each node.
+
+        Returns
+        -------
+            None
+        """
+
+        # initialize Graph object
+        G=nx.Graph()
+        
+        for node in self.nodes:
+            G.add_node(node, pos=(globals()[node].t, globals()[node].value), label=f"{round(globals()[node].value, 2)}\n{round(globals()[node].ev, 2)}")
+            for child in globals()[node].children:
+                G.add_edge(node, child.name, label=round(globals()[node].children[child], 2))
+        
+        nx.draw(G, pos=nx.get_node_attributes(G, 'pos'), with_labels = False)
+
+        nx.draw_networkx_edge_labels(G, pos=nx.get_node_attributes(G, 'pos'), label_pos=0.3,
+            edge_labels=nx.get_edge_attributes(G,'label'))
+        
+        nx.draw_networkx_labels(G, pos=nx.get_node_attributes(G, 'pos'),
+            labels=nx.get_node_attributes(G, 'label'))
+
+        # TODO: add annotation containing information about the option calculated
+        #annotation_text_x = max([i for i in nx.get_node_attributes(G, 'pos')])
+        # plt.text(0, annotation_text_x, "Annotation")
+        
+        plt.show()
+        
+        return
+
+    def __str__(self):
+        return(str(self.edgelist))
---
+++ misc/visualizing.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.colors as colors
+import random
+import numpy as np
+
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
+
+def draw_graph(bins1, bins2, S):
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(211)
+
+    for x in bins1.keys():
+        r = random.random()
+        b = random.random()
+        g = random.random()
+        color = (r, g, b)
+        for idx, i in enumerate(bins1[x].values()):
+            ax1.add_patch(
+                    patches.Rectangle((idx-0.5, min(i)), 1, max(i)-min(i), color=color, alpha=.5)
+                    )
+            # ax1.violinplot(i, positions=[idx])
+
+    ax1.plot(S, alpha=.05, color="black")
+    ax1.grid(True)
+    ax1.set_ylim((0,200))
+    ax1.set_xlim((0, len(bins1[list(bins1.keys())[0]].keys())))
+    ax1.set_xlabel("timesteps")
+    ax1.set_ylabel("Index Value")
+
+    if bins2:
+        ax2 = fig1.add_subplot(212)
+        for x in bins2.keys():
+            r = random.random()
+            b = random.random()
+            g = random.random()
+            color = (r, g, b)
+            for idx, i in enumerate(bins2[x].values()):
+                ax2.add_patch(
+                        patches.Rectangle((idx-0.5, min(i)), 1, max(i)-min(i), color=color, alpha=.5)
+                        )
+                # ax2.violinplot(i, positions=[idx])
+
+        ax2.plot(S, alpha=.05, color="black")
+        ax2.grid(True)
+        ax2.set_ylim((0, 200))
+        ax2.set_xlim((0, len(bins2[list(bins2.keys())[0]].keys())))
+        ax2.set_xlabel("Timestep")
+        ax2.set_ylabel("Index Value")
+
+    plt.tight_layout()
+    #plt.figtext(0, 0.001, "time m1: \ntime m2:", wrap=True)
+    plt.show()
+
+def draw_overlap(bins1, bins2, S=None, bin=[0], label= ["bins1", "bins2"], title=""):
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(211)
+    for x in bins1.keys(): #bins
+        if x in ["".join(["bin", str(i)]) for i in bin]:
+            for idx, (i, j) in enumerate(zip(bins1[x].values(), bins2[x].values())): #idx=t
+                    ax1.add_patch(
+                            patches.Rectangle((idx-0.5, min(i)), 0.5, max(i)-min(i), color=(1.0, 0.84, 0.17), alpha=0.5, label=label[0] if idx==0 else None)
+                            )
+                    ax1.add_patch(
+                            patches.Rectangle((idx, min(j)), 0.5, max(j)-min(j), color=(0.35, 0.36, 0.91), alpha=0.5, label=label[1] if idx==0 else None)
+                            )
+    if S is not None:
+        ax1.plot(S, alpha=.5, color="black")
+    ax1.grid(True)
+    ax1.set_ylim((0, 200))
+    ax1.set_xlim((0, len(bins1[list(bins1.keys())[0]].keys())))
+    ax1.set_xlabel("Timestep")
+    ax1.set_ylabel("Index Value")
+    fig1.legend(loc=7)
+    plt.title(title)
+    plt.tight_layout()
+    #plt.figtext(0, 0.001, "time m1: \ntime m2:", wrap=True)
+    plt.show()
+
+def draw_diffs(diffs_dict):
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+    for bin in diffs_dict.keys():
+        ax1.plot(diffs_dict[bin], label=f"{bin}")
+    ax1.set_xlabel("timesteps")
+    ax1.set_ylabel("absolute difference")
+    fig1.legend(loc=7)
+    plt.show()
