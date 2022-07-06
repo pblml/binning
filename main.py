d0b396daf9125ddf4467d0bd7651e9f3a761b8d5
pblml <philipp.t.blueml@gmail.com>
2022-07-06 15:47:29 +0200

    rewrite main

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ sim_fixed=Simulation().generate().binning(method="fixed", nbins=10).create_edgel
 
 # Initialisieren des Baums mit den Simulationsobjekten
 tree = Tree().from_S(sim_fixed)
-# tree.calc_option_value(104, 0.9752, "c") #12.85 fixed 13.309 kmeans
-# tree.plot()
+tree.calc_option_value(104, 0.9752, "c") #12.85 fixed 13.309 kmeans
+tree.plot()
 
-# tree.from_S(sim_kmeans)
-# tree.calc_option_value(104, 0.9752, "c") #12.85 fixed 13.309 kmeans
-# tree.plot()
+tree.from_S(sim_kmeans)
+tree.calc_option_value(104, 0.9752, "c") #12.85 fixed 13.309 kmeans
+tree.plot()
 
 # Initialisieren des Baums mittels einer Liste von Kanten
 
